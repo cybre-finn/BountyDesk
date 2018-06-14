51c68e6a7452a3426aaf8b23810a28b7fe98c8dc
Ikarulus <github.ikarulus@zer.one>
2018-06-14 11:38:24 +0200

    [FIX] 2cd6327 broke Ticket submission

--- public/views/CreateTicketView.js
+++ public/views/CreateTicketView.js
@@ -10,7 +10,7 @@ define([
     var CreateTicketView = Backbone.View.extend({
 
         initialize: function () {
-            
+
             this.TicketCollection = new TicketCollection({});
             this.RoomCollection = new RoomCollection({});
             var self = this;
@@ -35,9 +35,28 @@ define([
         },
 
         onCreateTicket: function (e) {
-            console.log(e);
             e.preventDefault();
             if (e.target.checkValidity() === true) {
+                this.TicketCollection.create({
+                    issuer: this.$("#CreateTicket-issuer").val(),
+                    headline: this.$("#CreateTicket-headline").val(),
+                    room: this.$("#CreateTicket-room").val(),
+                    content: this.$("#CreateTicket-content").val(),
+                    contact_email: this.$("#CreateTicket-email").val()
+                }, {
+                        success: function (model, response) {
+                            if (app.session.get("logged_in")) {
+                                app.showAlert("Ticket created.", "alert-success", 0);
+                            }
+                            else {
+                                app.showAlert("Ticket created. Keep track of your mailbox for updates from us.", "alert-success", 0);
+                            }
+                            Backbone.history.navigate('/', true);
+                        },
+                        error: function (model, response) {
+                            app.showAlert(response.status, "alert-danger");
+                        }
+                    });
             }
             else {
                 e.target.classList.add('was-validated');
